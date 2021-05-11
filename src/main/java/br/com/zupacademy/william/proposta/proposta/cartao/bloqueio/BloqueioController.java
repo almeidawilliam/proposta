626eec16e1c9c5b3936418066a3c8a08e6112205
William Almeida <almeida.ws@hotmail.com>
2021-05-11 13:44:52 -0300

    fix: remocao de system.out.print

--- src/main/java/br/com/zupacademy/william/proposta/proposta/cartao/bloqueio/BloqueioController.java
+++ src/main/java/br/com/zupacademy/william/proposta/proposta/cartao/bloqueio/BloqueioController.java
@@ -49,8 +49,6 @@ public class BloqueioController {
                             new BloqueioCartaoGatewayRequest("sistema de propostas")
                     );
 
-            System.out.println("gatewayResponse.getResultado() = " + gatewayResponse.getResultado());
-
             if (gatewayResponse.getResultado().equals(BloqueioCartaoResultado.BLOQUEADO)) {
                 //        String ip = request.getHeader("x-forwarded-for");
                 String remoteAddr = request.getRemoteAddr();
